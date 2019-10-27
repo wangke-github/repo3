82bafa69702d2f9d66dacc9615f48dfa0d783ba0
wangke <1321116843@qq.com>
2019-10-27 23:22:21 +0800

    创建gitdemo

---
+++ gitdemo/src/com/itcast/hello.java
@@ -0,0 +1,9 @@
+package com.itcast;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+        System.out.println("aaaa");
+    }
+}
